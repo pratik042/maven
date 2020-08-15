9fc2d483a909fa65deb19303a119c14509bf6ed6
Pratik Gole <pratikgole5682@gmail.com>
2020-08-15 19:23:47 +0530

    obj to string1

---
+++ A_MAVEN/src/main/java/all_abt_String/obj_string1.java
@@ -0,0 +1,28 @@
+package all_abt_String;
+//here we will convert the object to string
+//if we override the toString() method of java obj then we can get the String form of object
+
+
+public class obj_string1 {
+int i;
+String name;
+	obj_string1(int i,String name)//2) we made constructor
+	{this.i=i;//3) initialised values
+	 this.name=name;
+     }
+	
+	public String toString()//4) so we took help of toString method
+	{
+		return name+"  "+i;//5) and returned values
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		obj_string1 obj=new obj_string1(42,"pratik");// 1)we need to initialise so we make a constructor
+		//System.out.println(obj.toString());
+		System.out.println(obj);// 3) so as we see we got the hashcode of the data but we need in string form,so to string method comes here
+		                                              //4) now we got values
+	}
+
+}
